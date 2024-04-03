0fc2a786e3accc9f3a907860cb343b08f6619df4
loulidev <e.p.deligianni@gmail.com>
2024-04-03 17:06:58 +0300

    Updates Tabs stories

---
+++ src/stories/Tabs.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Tabs, { TabPanel, Tab } from "./Tabs";
+
+const meta: Meta<typeof Tabs> = {
+  title: "Example/Tabs",
+  component: Tabs,
+  subcomponents: { Tab, TabPanel },
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+  args: { defaultActiveId: "1" },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  render: (args) => (
+    <Tabs {...args}>
+      <Tab id="1" label="Tab 1" />
+      <TabPanel whenActive="1">Content for Tab 1</TabPanel>
+      <Tab id="2" label="Tab 2" />
+      <TabPanel whenActive="2">Content for Tab 2</TabPanel>
+    </Tabs>
+  ),
+};
