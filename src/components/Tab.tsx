c546029ba088594483b6f5672cf369c998ff48f3
loulidev <e.p.deligianni@gmail.com>
2024-04-10 16:25:27 +0300

    Adds onClick prop to Tab component

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -4,12 +4,14 @@ import ActiveTabContext from "../context/ActiveTabContext";
 interface TabProps {
   id: string;
   label: string;
+  onClick?: () => void;
 }
 
-export const Tab: React.FC<TabProps> = ({ id, label }) => {
+export const Tab: React.FC<TabProps> = ({ id, label, onClick }) => {
   const [, setActiveId] = useContext(ActiveTabContext);
 
   const handleOnClick = () => {
+    onClick && onClick();
     setActiveId(id);
   };
   return (
