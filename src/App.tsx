c2be2a359db7c99fd76f73a146a43f04fe2d7ec3
loulidev <e.p.deligianni@gmail.com>
2024-04-17 16:27:03 +0300

    Adds styles to Tab component

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
     <>
       <Tabs defaultActiveId={"1"}>
         <Tab id="1" label="Tab 1" />
-        <TabPanel whenActive="1">Content for Tab 1</TabPanel>
         <Tab id="2" label="Tab 2" />
+        <TabPanel whenActive="1">Content for Tab 1</TabPanel>
         <TabPanel whenActive="2">Content for Tab 2</TabPanel>
       </Tabs>
     </>
--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,21 +1,28 @@
 import { useContext } from "react";
 import ActiveTabContext from "../context/ActiveTabContext";
+import "../styles/TabStyles.css";
 
 interface TabProps {
   id: string;
   label: string;
   onClick?: () => void;
+  style?: React.CSSProperties;
 }
 
-export const Tab: React.FC<TabProps> = ({ id, label, onClick }) => {
-  const [, setActiveId] = useContext(ActiveTabContext);
+export const Tab: React.FC<TabProps> = ({ id, label, onClick, style }) => {
+  const [activeId, setActiveId] = useContext(ActiveTabContext);
 
   const handleOnClick = () => {
     onClick && onClick();
     setActiveId(id);
   };
   return (
-    <button type="button" onClick={handleOnClick}>
+    <button
+      type="button"
+      onClick={handleOnClick}
+      className={`tab-button ${activeId === id ? "active" : ""}`}
+      style={style}
+    >
       {label}
     </button>
   );
