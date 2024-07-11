87bc58a5fd1057fd799f4315cdb438090bbe5877
loulidev <e.p.deligianni@gmail.com>
2024-07-11 16:24:34 +0300

    Updates stories

--- src/stories/Tabs.stories.tsx
+++ src/stories/Tabs.stories.tsx
@@ -19,16 +19,55 @@ export const Primary: Story = {
   render: (args) => (
     <Tabs {...args}>
       <TabsList>
-        <Tab id="1" label="Tab 1" />
-        <span>🚀</span>
-        <Tab id="2" label="Tab 2" />
-        <Tab id="3" label="Tab 3" />
-        <Tab id="4" label="Tab 4" />
+        <Tab id="1">Tab 1</Tab>
+        <Tab id="2">Tab 2</Tab>
+        <Tab id="3">Tab 3</Tab>
       </TabsList>
       <TabPanel whenActive="1">Content for Tab 1</TabPanel>
       <TabPanel whenActive="2">Content for Tab 2</TabPanel>
       <TabPanel whenActive="3">Content for Tab 3</TabPanel>
-      <TabPanel whenActive="4">Content for Tab 4</TabPanel>
     </Tabs>
   ),
+};
+
+export const Icon: Story = {
+  render: (args) => (
+    <Tabs {...args}>
+      <TabsList>
+        <Tab id="1">
+          Tab 1<span>🔥</span>
+        </Tab>
+        <Tab id="2">
+          Tab 1<span>💧</span>
+        </Tab>
+        <Tab id="3">
+          Tab 3<span>🌳</span>
+        </Tab>
+      </TabsList>
+      <TabPanel whenActive="1">Content for Tab 1</TabPanel>
+      <TabPanel whenActive="2">Content for Tab 2</TabPanel>
+      <TabPanel whenActive="3">Content for Tab 3</TabPanel>
+    </Tabs>
+  ),
+};
+
+export const Disabled: Story = {
+  render: (args) => (
+    <Tabs {...args}>
+      <TabsList>
+        <Tab id="1">Tab 1</Tab>
+        <Tab id="2" disabled>
+          Tab 2
+        </Tab>
+        <Tab id="3">Tab 3</Tab>
+      </TabsList>
+      <TabPanel whenActive="1">Content for Tab 1</TabPanel>
+      <TabPanel whenActive="2">Content for Tab 2</TabPanel>
+      <TabPanel whenActive="3">Content for Tab 3</TabPanel>
+    </Tabs>
+  ),
+};
+
+export const Scrollable: Story = {
+  render: () => <div>Coming Soon...</div>,
 };
